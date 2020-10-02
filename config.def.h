3f5de05a79166ded6a614a0f45d3ca77d6d084ba
Oemer Yildiz <oemer.yildiz@tuhh.de>
2020-10-02 13:02:51 +0200

    hotkeys for terminal tools

--- config.def.h
+++ config.def.h
@@ -58,11 +58,19 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_purple, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *emacscmd[]  = { "emacs", NULL };
+static const char *fmcmd[]  = { "st", "ranger", NULL };
+static const char *monitorcmd[]  = { "st", "htop", NULL };
+static const char *mixercmd[]  = { "st", "alsamixer", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_F1,     spawn,          {.v = emacscmd } },
+	{ MODKEY,                       XK_F9,     spawn,          {.v = fmcmd } },
+	{ MODKEY,                       XK_F10,    spawn,          {.v = monitorcmd } },
+	{ MODKEY,                       XK_F11,    spawn,          {.v = mixercmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },

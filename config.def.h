9978b18ac7d40b97c4822dbed237a6f709902bdb
Oemer Yildiz <oemer.yildiz@tuhh.de>
2021-01-04 16:07:43 +0100

    add browser hotkey

--- config.def.h
+++ config.def.h
@@ -59,6 +59,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_purple, "-sf", col_gray4, NULL };
+static const char *browsercmd[]  = { "qutebrowser", NULL };
 static const char *eclientcmd[]  = { "emacsclient", "-c", NULL };
 static const char *emacscmd[]  = { "emacs", NULL };
 static const char *fmcmd[]  = { "st", "ranger", NULL };
@@ -81,7 +82,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_F9,     spawn,          {.v = monitorcmd } },
 	{ MODKEY,                       XK_F10,    spawn,          {.v = fmcmd } },
 	{ MODKEY,                       XK_F11,    spawn,          {.v = eclientcmd } },
-	{ MODKEY,                       XK_F12,    spawn,          {.v = emacscmd } },
+	{ MODKEY|ShiftMask,             XK_F11,    spawn,          {.v = emacscmd } },
+	{ MODKEY,                       XK_F12,    spawn,          {.v = browsercmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
